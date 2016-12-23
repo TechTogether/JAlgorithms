37d9b1f9e80c791dd7dbdf85e809d3a3806622a0
Sumit Kawatra <sumitkkawatra@gmail.com>
2016-12-23 22:55:01 +0530

    Tree  ADT node class created.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # JAlgorithms
 Data Structure and Algorithms implementation using Java.
 
-#LinkedList ADT Class
-we.techtogether.ds.node.ListNode
+LinkedList ADT Class
+	> we.techtogether.ds.node.ListNode
+
+Tree ADT Class
+	> we.techtogether.ds.node.TreeNode
---
+++ src/main/java/we/techtogether/ds/node/TreeNode.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package we.techtogether.ds.node;
+
+/**
+ * @author tech-together
+ *
+ */
+public class TreeNode<T extends Comparable<T>> {
+
+	private TreeNode<T> left;
+	private T info;
+	private TreeNode<T> right;
+
+	public TreeNode() {
+		this(null,null,null);
+	}
+	
+	public TreeNode(T info) {
+		this(null,info,null);
+	}
+
+	public TreeNode(TreeNode<T> left, T info, TreeNode<T> right) {
+		super();
+		this.left = left;
+		this.info = info;
+		this.right = right;
+	}
+
+	/**
+	 * @return the left
+	 */
+	public TreeNode<T> getLeft() {
+		return left;
+	}
+
+	/**
+	 * @param left the left to set
+	 */
+	public void setLeft(TreeNode<T> left) {
+		this.left = left;
+	}
+
+	/**
+	 * @return the info
+	 */
+	public T getInfo() {
+		return info;
+	}
+
+	/**
+	 * @param info the info to set
+	 */
+	public void setInfo(T info) {
+		this.info = info;
+	}
+
+	/**
+	 * @return the right
+	 */
+	public TreeNode<T> getRight() {
+		return right;
+	}
+
+	/**
+	 * @param right the right to set
+	 */
+	public void setRight(TreeNode<T> right) {
+		this.right = right;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "TreeNode [leftChild=" + ((null == left) ? null:left.getInfo()) + ", info=" + info + ", rightChild=" + ((null == right)  ? null:right.getInfo()) + "]";
+	}
+
+}
