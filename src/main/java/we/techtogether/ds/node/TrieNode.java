4bb571c122c53b333ef683f92420189cb9f46661
Sumit Kawatra <sumitkkawatra@gmail.com>
2016-12-31 19:33:43 +0530

    Trie Node Datastructure.

---
+++ src/main/java/we/techtogether/ds/node/TrieNode.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package we.techtogether.ds.node;
+
+/**
+ * @author sumitkawatra
+ *
+ */
+public class TrieNode {
+
+	/**
+	 * 
+	 */
+	public TrieNode() {
+		// TODO node of text trie ds.
+	}
+
+}
